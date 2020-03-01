d012ac6716855980f0227052f8fd23c940092707
zhandonbi <zhandonbi@163.com>
2020-03-01 10:42:12 +0800

    完成呢部分接口

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from db_operator.item_db import ItemDb
+from ASR.ASR import ASR
 
 app = Flask(__name__)
 
@@ -8,25 +9,43 @@ if __name__ == '__main__':
 
 
 # 此行以下编辑你的代码
-@app.route('/get_classID/',methods=['GET'])
+@app.route('/get_classID/', methods=['GET'])
 def get_classID():
     return {
-        '1':'可回收垃圾',
-        '2':'其他垃圾',
-        '3':'有害垃圾',
-        '4':'厨余垃圾'
+        '1': '可回收垃圾',
+        '2': '其他垃圾',
+        '3': '有害垃圾',
+        '4': '厨余垃圾'
     }
+
 @app.route('/exact_search/', methods=['GET', 'POST'])
 def exact_search():
     result = {}
     if request.method == 'POST':
         item_name = request.form['item_name']
         item = ItemDb()
-        return item.item_search_exact(item_name)
+        result = item.item_search_exact(item_name)
+        item.close()
     else:
-        return {'接口返回示例': {'ID': 5249, 'Name': '鸡蛋', 'ClassID': 4},
-                '如果您发出的物品名不存在': {'ID': -1, 'Name': "NOT EXIST", 'ClassID': -1}
-                }
+        result = {'接口返回示例': {'ID': 5249, 'Name': '鸡蛋', 'ClassID': 4},
+                  '如果您发出的物品名不存在': {'ID': -1, 'Name': "NOT EXIST", 'ClassID': -1}
+                  }
+    return result
 
 @app.route('/exact_search_ID/', methods=['GET', 'POST'])
 def exact_search_ID():
+    result = {}
+    if request.method == 'POST':
+        item_ID = request.form['item_ID']
+        item = ItemDb()
+        result = item.item_search_exact_ID(item_ID)
+        item.close()
+    else:
+        result = {'接口返回示例': {'ID': 5249, 'Name': '鸡蛋', 'ClassID': 4},
+                  '如果您发出的物品名不存在': {'ID': -1, 'Name': "NOT EXIST", 'ClassID': -1}
+                  }
+    return result
+
+@app.route('/vague_search/', methods=['GET', 'POST'])
+def vague_search():
+    pass
