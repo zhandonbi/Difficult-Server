a65faa82a7d24f179469d2c1f7b9378ae470f147
zhandonbi <zhandonbi@163.com>
2020-03-31 14:49:09 +0800

    AIR

--- AIR/AIR.py
+++ AIR/AIR.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import os
 from tensorflow.python.framework import graph_util
 
-session = tf.Session()
-op = tf.add()
+# session = tf.Session()
+# op = tf.add()
 
 def AIR(image):
     return {'ID': -1, 'Name': str(image.filename), 'classID': -1}
