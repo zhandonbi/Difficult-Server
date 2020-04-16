c854eaaefbba0a348f6017fc2e47f62ec95aa049
zhandonbi <zhandonbi@163.com>
2020-04-16 17:25:42 +0800

    testAIR

--- AIR/AIR.py
+++ AIR/AIR.py
@@ -12,30 +12,36 @@ def temp_AIR(image):
         'img': post_image
     }
     res = requests.post(url=url, data=datas).text
-    # res = '{"code":200,"msg":"success","newslist":[{"name":"塑料盒","trust":73,"lajitype":0,"lajitip":"塑料盒是可回收垃圾，常见包括各类废金属、玻璃瓶、饮料瓶、电子产品等。投放时应注意轻投轻放、清洁干燥、避免污染。"},{"name":"手机后盖","trust":50,"lajitype":4,"lajitip":"手机后盖的垃圾分类系统暂时无法判别，请重新尝试拍摄物体的主要特征。"},{"name":"平板手机","trust":33,"lajitype":0,"lajitip":"平板手机是可回收垃圾，常见包括各类废金属、玻璃瓶、饮料瓶、电子产品等。投放时应注意轻投轻放、清洁干燥、避免污染。"},{"name":"一次性餐具","trust":16,"lajitype":3,"lajitip":"一次性餐具是其它干垃圾，常见包括砖瓦陶瓷、卫生间废纸、猫砂、毛发、一次性制品等。投放时应尽量沥干水分、平整轻放。"},{"name":"包装袋\/盒","trust":1,"lajitype":4,"lajitip":"包装袋\/盒的垃圾分类系统暂时无法判别，请重新尝试拍摄物体的主要特征。"}]}'
+    # res = '{"code":200,"msg":"success","newslist":[{"name":"鼠标","trust":89,"lajitype":0,"lajitip":"鼠标是可回收垃圾，常见包括各类废金属、玻璃瓶、饮料瓶、电子产品等。投放时应注意轻投轻放、清洁干燥、避免污染。"},{"name":"摩托车","trust":57,"lajitype":4,"lajitip":"摩托车的垃圾分类系统暂时无法判别，请重新尝试拍摄物体的主要特征。"},{"name":"电脑外设","trust":38,"lajitype":4,"lajitip":"电脑外设的垃圾分类系统暂时无法判别，请重新尝试拍摄物体的主要特征。"},{"name":"铜斑蛇","trust":19,"lajitype":4,"lajitip":"铜斑蛇的垃圾分类系统暂时无法判别，请重新尝试拍摄物体的主要特征。"},{"name":"锹形虫","trust":0,"lajitype":4,"lajitip":"锹形虫的垃圾分类系统暂时无法判别，请重新尝试拍摄物体的主要特征。"}]}'
     get_res = json.loads(res)
     result = {}
     if int(get_res['code']) == 200:
         item_list = list(get_res['newslist'])
         i = 0
+        ID = -1
         for item in item_list:
-            if i >= 3:
-                break
-            ClassID = int(item['lajitype'])
-            if ClassID == 0:
-                ClassID = 1
-            elif ClassID == 1:
-                ClassID = 3
-            elif ClassID == 2:
-                ClassID = 4
-            elif ClassID == 3:
-                ClassID = 2
-            temp = {
-                'ClassID': ClassID,
-                'Name': item['name']
-            }
-            result[str(i)] = temp
-            i +=1
+            if int(item['lajitype']) != 4:
+                if i >= 3:
+                    break
+                ClassID = int(item['lajitype'])
+                # 可回收
+                if ClassID == 0:
+                    ID = 1
+                # 有害
+                elif ClassID == 1:
+                    ID = 3
+                # 厨余垃圾
+                elif ClassID == 2:
+                    ID = 4
+                # 其他垃圾
+                elif ClassID == 3:
+                    ID = 2
+                temp = {
+                    'ClassID': ID,
+                    'Name': item['name']
+                }
+                result[str(i)] = temp
+                i += 1
         print(res)
         return result
     else:
