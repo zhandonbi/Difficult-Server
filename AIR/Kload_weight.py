5d107f48426f85a1f697e04c8912b36a27dcc6d7
zhandonbi <zhandonbi@163.com>
2020-05-08 10:26:52 +0800

    testAIR

---
+++ AIR/Kload_weight.py
@@ -0,0 +1,98 @@
+from keras_efficientnets import EfficientNetB5
+import numpy as np
+
+label_id_name_dict = \
+    {
+        "0": "其他垃圾/一次性快餐盒",
+        "1": "其他垃圾/污损塑料",
+        "2": "其他垃圾/烟蒂",
+        "3": "其他垃圾/牙签",
+        "4": "其他垃圾/破碎花盆及碟碗",
+        "5": "其他垃圾/竹筷",
+        "6": "厨余垃圾/剩饭剩菜",
+        "7": "厨余垃圾/大骨头",
+        "8": "厨余垃圾/水果果皮",
+        "9": "厨余垃圾/水果果肉",
+        "10": "厨余垃圾/茶叶渣",
+        "11": "厨余垃圾/菜叶菜根",
+        "12": "厨余垃圾/蛋壳",
+        "13": "厨余垃圾/鱼骨",
+        "14": "可回收物/充电宝",
+        "15": "可回收物/包",
+        "16": "可回收物/化妆品瓶",
+        "17": "可回收物/塑料玩具",
+        "18": "可回收物/塑料碗盆",
+        "19": "可回收物/塑料衣架",
+        "20": "可回收物/快递纸袋",
+        "21": "可回收物/插头电线",
+        "22": "可回收物/旧衣服",
+        "23": "可回收物/易拉罐",
+        "24": "可回收物/枕头",
+        "25": "可回收物/毛绒玩具",
+        "26": "可回收物/洗发水瓶",
+        "27": "可回收物/玻璃杯",
+        "28": "可回收物/皮鞋",
+        "29": "可回收物/砧板",
+        "30": "可回收物/纸板箱",
+        "31": "可回收物/调料瓶",
+        "32": "可回收物/酒瓶",
+        "33": "可回收物/金属食品罐",
+        "34": "可回收物/锅",
+        "35": "可回收物/食用油桶",
+        "36": "可回收物/饮料瓶",
+        "37": "有害垃圾/干电池",
+        "38": "有害垃圾/软膏",
+        "39": "有害垃圾/过期药物"
+    }
+
+
+def load_model():
+    model = EfficientNetB5(weights=None,
+                           include_top=False,
+                           input_shape=(456, 456, 3),
+                           classes=40,
+                           pooling=max)
+    model.load_weights('AIR/weights_028_0.5235.h5')
+    return model
+
+
+model = load_model()
+
+
+def center_img(img, size=None, fill_value=255):
+    h, w = img.shape[:2]
+    if size is None:
+        size = max(h, w)
+    shape = (size, size) + img.shape[2:]
+    background = np.full(shape, fill_value, np.uint8)
+    center_x = (size - w) // 2
+    center_y = (size - h) // 2
+    background[center_y:center_y + h, center_x:center_x + w] = img
+    return background
+
+
+def preprocess_img(img):
+    resize_scale = 456 / max(img.size[:2])
+    img = img.resize(
+        (int(img.size[0] * resize_scale), int(img.size[1] * resize_scale)))
+    img = img.convert('RGB')
+    img = np.array(img)
+    img = img[:, :, ::-1]
+    img = center_img(img, 456)
+    img = img[np.newaxis, :, :, :]
+    img = img.astype(img, np.float32) / 225.0
+    mean = [0.56719673, 0.5293289, 0.48351972]
+    std = [0.20874391, 0.21455203, 0.22451781]
+    img[..., 0] -= mean[0]
+    img[..., 1] -= mean[1]
+    img[..., 2] -= mean[2]
+    img[..., 0] /= std[0]
+    img[..., 1] /= std[1]
+    img[..., 2] /= std[2]
+    return img
+
+
+def run(image):
+    image = preprocess_img(image)
+    result = model.predict(image)
+    print(result)
--- test_function.py
+++ test_function.py
@@ -2,9 +2,9 @@
 # 为防止文件路径以及其它问题，请在此文件内测试您的功能模块
 from db_operator.item_db import ItemDb
 from ASR.ASR import ASR
+from AIR.AIR import *
 
 if __name__ == '__main__':
     print('正在运行功能测试，注意不要使用在正式环境中')
-    IT = ItemDb()
-
-    print(IT.items_read_all('0'))
+    with open('AIR/SAVE_PB/fimg_0.jpg','rb') as f:
+        print(temp_AIR(f.read()))
